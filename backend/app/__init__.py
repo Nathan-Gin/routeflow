206da6fcc6a8da9349fa8ed01b4f6a05cb76af80
Nathan Ginnifer <nginnifer@gmail.com>
2026-09-06 16:01:19 +0100

    Added basic frontend functionality with backend communcation

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,27 @@
-from flask import Flask, request
+from flask import Flask, send_from_directory
 from app.routes import routes
+from pathlib import Path
 
 
 def create_app():
     app = Flask(__name__)
 
+    app.register_blueprint(routes)
+
+    frontend_path = Path(__file__).resolve().parents[2] / "frontend"
+
     @app.get("/")
     def home():
-        return {"message": "Welcome to RouteFlow"}
+        return send_from_directory(frontend_path, "index.html")
 
-    app.register_blueprint(routes)
+    """When the broswer searches
+    for a path e.g styles.css it will automatically
+    search the current directory this ensures we instead
+    when sending get requests our route tells us to
+    look in the /frontend directory
+    """
+    @app.get("/<path:filename>")
+    def frontend_files(filename):
+        return send_from_directory(frontend_path, filename)
 
     return app
